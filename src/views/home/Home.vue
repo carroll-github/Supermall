3045b5f831555f3e31ba71a8bcbe485d820add51
carroll <1013602082@qq.com>
2020-09-14 10:25:43 +0800

    初始化项目

---
+++ src/views/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>这是首页</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    components: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
